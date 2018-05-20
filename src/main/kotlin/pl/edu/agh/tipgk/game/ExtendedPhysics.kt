d54d65635c36ad7d21da1c8396d6e35bee90fe9e
Borekk <karbor@student.agh.edu.pl>
2018-05-20 21:07:21 +0200

    Revert "Exercise three"
    This reverts commit 7f7af2f

--- src/main/kotlin/pl/edu/agh/tipgk/game/ExtendedPhysics.kt
+++ src/main/kotlin/pl/edu/agh/tipgk/game/ExtendedPhysics.kt
@@ -2,6 +2,7 @@ package pl.edu.agh.tipgk.game
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.graphics.*
 import com.badlogic.gdx.graphics.g2d.Sprite
@@ -21,6 +22,8 @@ class ExtendedPhysics : ApplicationAdapter(), InputProcessor {
     private lateinit var camera: OrthographicCamera
 
     private var torque = 0.0f
+    private var elapsed = 0.0f
+    private var drawSprite = true
 
     companion object {
         var PIXELS_TO_METERS = 100f
@@ -53,9 +56,6 @@ class ExtendedPhysics : ApplicationAdapter(), InputProcessor {
         body.createFixture(fixtureDef)
         shape.dispose()
 
-        //init transform
-        body.setTransform(0f, 0f, 0f)
-
         Gdx.input.inputProcessor = this
         camera = OrthographicCamera(Gdx.graphics.width.toFloat()
                 , Gdx.graphics.height.toFloat())
@@ -78,21 +78,32 @@ class ExtendedPhysics : ApplicationAdapter(), InputProcessor {
         spriteBatch.projectionMatrix = camera.combined
 
         debugMatrix = spriteBatch.projectionMatrix.cpy().scale(PIXELS_TO_METERS,
-                PIXELS_TO_METERS, 0f)
+                PIXELS_TO_METERS, 0f);
 
         spriteBatch.begin()
 
-        spriteBatch.draw(sprite, sprite.x, sprite.y, sprite.originX,
-                sprite.originY,
-                sprite.width,
-                sprite.height, sprite.scaleX,
-                sprite.scaleY, sprite.rotation)
+        if (drawSprite)
+            spriteBatch.draw(sprite, sprite.x, sprite.y, sprite.originX,
+                    sprite.originY,
+                    sprite.width,
+                    sprite.height, sprite.scaleX,
+                    sprite.scaleY, sprite.rotation)
 
         spriteBatch.end()
     }
 
     override fun dispose() {
         world.dispose()
+    }
+
+    private fun drawCircle(): Texture {
+        val pixmap = Pixmap(150, 150, Pixmap.Format.RGBA8888)
+        pixmap.setColor(Color.BROWN)
+        pixmap.fillCircle(75, 75, 75)
+        val texture = Texture(pixmap)
+        pixmap.dispose()
+
+        return texture
     }
 
     fun drawRectangle(width: Int, height: Int): Texture {
@@ -122,12 +133,37 @@ class ExtendedPhysics : ApplicationAdapter(), InputProcessor {
     }
 
     override fun keyUp(keycode: Int): Boolean {
-        /*
-            Add linear velocity in each direction (Input.Keys.Right|LEFT)
-            Apply force for UP and DOWN (body.applyForceToCenter)
-            Increment/decrement torque
-            Reset velocity, force and torque on SPACE up
-         */
+        if (keycode == Input.Keys.RIGHT)
+            body.setLinearVelocity(1f, 0f)
+        if (keycode == Input.Keys.LEFT)
+            body.setLinearVelocity(-1f, 0f)
+
+        if (keycode == Input.Keys.UP)
+            body.applyForceToCenter(0f, 10f, true)
+        if (keycode == Input.Keys.DOWN)
+            body.applyForceToCenter(0f, -10f, true)
+
+        // On brackets ( [ ] ) apply torque, either clock or counterclockwise
+        if (keycode == Input.Keys.RIGHT_BRACKET)
+            torque += 0.1f
+        if (keycode == Input.Keys.LEFT_BRACKET)
+            torque -= 0.1f
+
+        // Remove the torque using backslash /
+        if (keycode == Input.Keys.BACKSLASH)
+            torque = 0.0f
+
+        // If user hits spacebar, reset everything back to normal
+        if (keycode == Input.Keys.SPACE) {
+            body.setLinearVelocity(0f, 0f)
+            body.setAngularVelocity(0f)
+            torque = 0f
+            sprite.setPosition(0f, 0f)
+            body.setTransform(0f, 0f, 0f)
+        }
+
+        if (keycode == Input.Keys.ESCAPE)
+            drawSprite = !drawSprite
 
         return true
     }
@@ -141,6 +177,7 @@ class ExtendedPhysics : ApplicationAdapter(), InputProcessor {
     }
 
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        body.applyForce(1f, 1f, screenX.toFloat(), screenY.toFloat(), true)
         return true
     }
 }
