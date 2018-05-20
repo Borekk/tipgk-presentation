b2e03325ce0937d1ae626394bb1e0c6f9a4998c9
Borekk <karbor@student.agh.edu.pl>
2018-05-20 20:02:25 +0200

    Exercise template

--- src/main/kotlin/pl/edu/agh/tipgk/Application.kt
+++ src/main/kotlin/pl/edu/agh/tipgk/Application.kt
@@ -1,13 +1,5 @@
 package pl.edu.agh.tipgk
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
-import pl.edu.agh.tipgk.game.Collisions
-
 fun main(args: Array<String>) {
-    val config = LwjglApplicationConfiguration()
-//    LwjglApplication(ExampleWindow(), config)
-//    LwjglApplication(BasicGravity(), config)
-//    LwjglApplication(ExtendedPhysics(), config)
-    LwjglApplication(Collisions(), config)
 }
+
