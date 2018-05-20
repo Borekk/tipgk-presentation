7190ce8052d909846b998f3c63f0735ce993ef70
Borekk <karbor@student.agh.edu.pl>
2018-05-20 20:52:43 +0200

    Exercise three solution

--- src/main/kotlin/pl/edu/agh/tipgk/Application.kt
+++ src/main/kotlin/pl/edu/agh/tipgk/Application.kt
@@ -2,9 +2,9 @@ package pl.edu.agh.tipgk
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
-import pl.edu.agh.tipgk.game.BasicGravity
+import pl.edu.agh.tipgk.game.ExtendedPhysics
 
 fun main(args: Array<String>) {
     val config = LwjglApplicationConfiguration()
-    LwjglApplication(BasicGravity(), config)
+    LwjglApplication(ExtendedPhysics(), config)
 }
