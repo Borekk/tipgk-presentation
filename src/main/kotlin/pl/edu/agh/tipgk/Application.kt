bd30fb26643dd2a8805f4b232d17bfec5190704d
Borekk <karbor@student.agh.edu.pl>
2018-05-20 20:32:20 +0200

    Exercise two - solution

--- src/main/kotlin/pl/edu/agh/tipgk/Application.kt
+++ src/main/kotlin/pl/edu/agh/tipgk/Application.kt
@@ -1,5 +1,10 @@
 package pl.edu.agh.tipgk
 
-fun main(args: Array<String>) {
-}
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import pl.edu.agh.tipgk.game.BasicGravity
 
+fun main(args: Array<String>) {
+    val config = LwjglApplicationConfiguration()
+    LwjglApplication(BasicGravity(), config)
+}
